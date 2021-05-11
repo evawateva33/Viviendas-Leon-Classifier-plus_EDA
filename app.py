ef04a39e0927240fb1c63104359bebe8dbe875c2
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-11 13:58:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def main():
                                      result=prediction,
                                      )
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=7525,debug=False)
+    app.run()
