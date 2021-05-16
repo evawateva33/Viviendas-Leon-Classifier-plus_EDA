98f3320a3c84a4d94d86b56dd11f21d178047640
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-16 13:17:09 -0700

    try again

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ import pickle
 # Use pickle to load in the pre-trained model.
 with open(f'model/farm2_model_xgboost.pkl', 'rb') as f:
     model = pickle.load(f)
-
+with open(f'model/finalized_cropmodel2.pkl', 'rb') as f:
+    model2 = pickle.load(f)
 
 app = flask.Flask(__name__, template_folder='templates')
 @app.route('/', methods=['GET', 'POST'])
@@ -47,7 +48,7 @@ def main():
 
 
 
-        
+        prediction2 = model2.predict(input_variables)[0]        
         return flask.render_template('main.html',
                                      original_input={'% Disease': Disease,
                                                      'Wellness_Condition':Wellness_Condition,
@@ -61,7 +62,7 @@ def main():
                                                     'Season_Summer':Season_Summer,
                                                     'Season_Winter':Season_Winter,
                                                     'Region_Goyena':Region_Goyena },
-                                     result=prediction,
+                                     result=prediction2,
                                      )
 if __name__ == '__main__':
     app.run()
