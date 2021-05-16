7fc0a1dc7cb25bb63125a9b4f74b609418ebea11
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-16 13:47:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ import os
  #app name
 import pickle
 # Use pickle to load in the pre-trained model.
-#with open(f'model/farm2_model_xgboost.pkl', 'rb') as f:
-    #model = pickle.load(f)
-with open(f'model/finalized_cropmodel2.pkl', 'rb') as f:
-    model2 = pickle.load(f)
+with open(f'model/farm2_model_xgboost.pkl', 'rb') as f:
+    model = pickle.load(f)
+#with open(f'model/finalized_cropmodel2.pkl', 'rb') as f:
+  #  model2 = pickle.load(f)
 
 app = flask.Flask(__name__, template_folder='templates')
 @app.route('/', methods=['GET', 'POST'])
@@ -44,11 +44,11 @@ def main():
                                                 'Season_Summer', 'Season_Winter', 'Region_Goyena', 
                                                 'Region_Troilo'],
                                        dtype=float)
-        #prediction = model.predict(input_variables)[0]
+        prediction = model.predict(input_variables)[0]
 
 
 
-        prediction2 = model2.predict(input_variables)[0]        
+        #prediction2 = model2.predict(input_variables)[0]        
         return flask.render_template('main.html',
                                      original_input={'% Disease': Disease,
                                                      'Wellness_Condition':Wellness_Condition,
@@ -62,7 +62,7 @@ def main():
                                                     'Season_Summer':Season_Summer,
                                                     'Season_Winter':Season_Winter,
                                                     'Region_Goyena':Region_Goyena },
-                                     result=prediction2,
+                                     result=prediction,
                                      )
 if __name__ == '__main__':
     app.run()
